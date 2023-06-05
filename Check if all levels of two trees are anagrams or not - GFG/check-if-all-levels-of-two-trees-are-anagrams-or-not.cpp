ec789378ff3105fc6ed7de7e3c0e4b43f3256f87
Kunal Bansal <80686922+KunalBansall@users.noreply.github.com>
2023-06-05 22:58:49 +0530

    Added solution - LeetHub

--- Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.cpp
+++ Check if all levels of two trees are anagrams or not - GFG/check-if-all-levels-of-two-trees-are-anagrams-or-not.cpp
@@ -81,11 +81,11 @@ Node* buildTree(string str) {
 
 class Solution{
     public:
-    bool isAnagram (vector<int> v1,vector <int> v2){
-        sort(v1.begin(),v1.end());
-        sort(v2.begin(),v2.end());
-  return v1==v2;
-     
+    bool isAnagram(vector<int>v1,vector<int>v2){
+        
+       sort(v1.begin(),v1.end());
+       sort(v2.begin(),v2.end());
+        return v1==v2;
     }
     bool areAnagrams(Node *root1, Node *root2)
     {
@@ -93,46 +93,49 @@ class Solution{
         queue<Node*> q2;
         q1.push(root1);
         q2.push(root2);
-         vector <int> v1,v2;
-        while(!q1.empty() &&!q2.empty()){
-            int n1 = q1.size();
-            int n2 = q2.size();
-               vector <int> v1,v2;
-            
-            if(n1!= n2){
-                return false;
-            }
-            for(int i=0;i<n1;i++){
-                Node* temp =q1.front();
-                q1.pop();
-                v1.push_back(temp->data);
-                if( temp->left){
-                    q1.push(temp->left);
-                }
-                if(temp->right){
-                    q1.push(temp->right);
-                }
-                
-                  Node* temp2 =q2.front();
-                q2.pop();
-                v2.push_back(temp2->data);
-                if( temp2->left){
-                    q2.push(temp2->left);
-                }
-                if(temp2->right){
-                    q2.push(temp2->right);
-                }
-                
-                
-            }
-            if(!isAnagram(v1,v2)){
-                return false;
-                
+        vector<int> v1,v2;
+        
+        
+        while(!q1.empty() && !q2.empty()){
+        int n1= q1.size();
+        int n2= q2.size();
+        
+        if(n1!=n2){
+            return false;
+        }
+        
+        for(int i=0;i<n1;i++){
+            Node* temp1= q1.front();
+            q1.pop();
+            v1.push_back(temp1->data);
+            if(temp1->left){
+                q1.push(temp1->left);
             }
             
+            if(temp1->right){
+                q1.push(temp1->right);
+            }
+            
+            Node* temp2= q2.front();
+            q2.pop();
+            v2.push_back(temp2->data);
+            if(temp2->left){
+                q2.push(temp2->left);
+            }
+            
+            if(temp2->right){
+                q2.push(temp2->right);
+            }
+            
+             
+        }
+        if(!isAnagram(v1,v2)){
+                return false;
+                
+            }
+    
         }
         return true;
-    
     }
 };
 
